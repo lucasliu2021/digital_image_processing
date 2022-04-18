a75553adfa9e6a5616696f09a4ac76010f6c833d
良浩 <60993837+lucasliu2021@users.noreply.github.com>
2022-04-18 17:35:57 +0800

    Update school1.m
    修复BUG

--- script/school1.m
+++ script/school1.m
@@ -1,55 +1,49 @@
-close all;
-clear;
-clc;
-%直观感受图像对比度较弱，细节看不清 ,使用彩色直方图均衡处理
-
-rgb=imread('school1.jpg');rgb1=im2double(rgb);
-r=rgb1(:,:,1);g=rgb1(:,:,2);b=rgb1(:,:,3);%%分别得到三色分量
-I1=(r+g+b)/3; %%计算HSI彩色模型的I分量
-tmp1=min(min(r,g),b);%计算RGB最小值
-tmp2=r+g+b;
-tmp2(tmp2==0)=eps;%避免除数为0
-S=1-3.*tmp1./tmp2;
+clear
+close all
+clc
+rgb=imread('school1.jpg') ;
+subplot(1,2,1);imshow(rgb);title('鍘熷鍥惧儚')
+rgb = im2double (rgb) ;
+r=rgb(:,:,1);
+g = rgb(:,:,2);
+b = rgb(:,:, 3) ;
 tmp1=0.5*((r-g)+(r-b));
 tmp2=sqrt((r-g).^2+(r-b).*(g-b));
-theta=acos(tmp1./(tmp2+eps));
-H1=theta;
-H1(b>g)=2*pi-H1(b>g);
-H1=H1/(2*pi);
-H1(S==0)=0;
-I=histeq(I1);
-hsi=cat(3,H1,S,I);
+theta=acos(tmp1./(tmp2 + eps) ) ;
+H = theta;
+H(b>g)=2*pi-H(b>g);
+H=H/ (2*pi) ;
+tmp1 = min(min (r,g),b) ;
+tmp2=r+g+b;
+tmp2 (tmp2 == 0) = eps;
+S = 1 - 3.* tmp1./ tmp2;
+H(S==0)=0;
+I =(r+g+b)/3;
+hsi= cat(3, H, S, I) ;
+hsi(:,:,3)=histeq(hsi(:,:,3));
 H=hsi(:,:,1)*2*pi;
 S=hsi(:,:,2);
 I=hsi(:,:,3);
-R=zeros(size(hsi,1),size(hsi,2));
-G=zeros(size(hsi,1),size(hsi,2));
-B=zeros(size(hsi,1),size(hsi,2));
-ind=find((H>=0)&(H<2*pi/3));
-B(ind)=I(ind).*(1.0-S(ind));
-R(ind)=I(ind).*(1.0+S(ind).*cos(H(ind))./cos(pi/3.0-H(ind)));
-G(ind)=3.0-(R(ind)+B(ind));
+R = zeros(size(hsi,1),size(hsi,2));
+G = zeros(size(hsi,1),size(hsi,2));
+B = zeros(size(hsi,1),size(hsi,2));
+ind=find((0<=H)&(H<2*pi/3));ss
+B(ind)=I(ind).*(1-S(ind));
+R(ind)=I(ind).*(1+S(ind).*cos(H(ind))./cos(pi/3-H(ind)));
+G (ind) = 3*I(ind) -(R(ind)+B(ind));
+ind = find( (2*pi/3 <= H) & (H < 4*pi/3));
+R(ind)=I(ind).*(1-S(ind));
+G(ind) = I (ind).* (1 + S(ind).*cos(H(ind)-2*pi/3)./cos (pi -H(ind)));
+B(ind)=3*I(ind)-(R(ind)+G(ind));
+ind=find((4*pi/3<=H)&(H<=2*pi));
+G(ind)=I(ind).*(1-S(ind));
+B(ind)=I(ind).*(1+S(ind).*cos(H(ind)-4*pi/3)./cos(5*pi/3-H(ind)));
+R (ind) = 3*I (ind) - (G(ind) + B(ind) ) ;
+RGB=uint8 (cat (3, R, G, B) *255) ;
+subplot(1,2,2);imshow(RGB);title('鏂扮殑鍥惧儚')
+imwrite(RGB,'change SCHOOL1.jpg');
 
 
-ind=find((H>2*pi/3)&(H<4*pi/3));
 
-H(ind)=H(ind)-pi*2/3;
-R(ind)=I(ind).*(1.0-S(ind));
-G(ind)=I(ind).*(1.0+S(ind).*cos(H(ind))./cos(pi/3.0-H(ind)));
-B(ind)=3.0-(R(ind)+G(ind));
 
-ind=find((H>=4*pi/3)&(H<2*pi));
 
-H(ind)=H(ind)-pi*4/3;
-G(ind)=I(ind).*(1.0-S(ind));
-B(ind)=I(ind).*(1.0+S(ind).*cos(H(ind)./cos(pi/3.0-H(ind))));
-R(ind)=3.0-(G(ind)+B(ind));
-RGB=cat(3,R,G,B);
-figure,imshow(H1);
-figure,imshow(I1);
-figure,imshow(I);
-figure,imshow(S);
-figure,imshow(rgb);
-figure,imshow(RGB);
-
-% 写入图像文件
